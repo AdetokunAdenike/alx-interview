e0cbfcfac2eb0be169779b2104cfb78d9ef6e00b
AdetokunAdenike <adetokunadenike@gmail.com>
2024-12-01 15:43:14 +0100

    Remove trailling white space

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,19 @@
-#!/usr/bin/python3
 def makeChange(coins, total):
+    """
+    Returns the fewest number of coins needed to make up a given total.
+
+    Parameters:
+    coins (list of int): The available coin denominations.
+    total (int): The target amount.
+
+    Returns:
+    int: The fewest number of coins needed, or -1 if the total cannot be made.
+
+    Example:
+    makeChange([1, 2, 25], 37)  # Returns 7
+    makeChange([1256, 54, 48, 16, 102], 1453)  # Returns -1
+    """
+
     if total <= 0:
         return 0
 
